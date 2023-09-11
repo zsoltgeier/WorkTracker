9c24fed6c4b7c26f4ccd3f14b28dfefbb1a2947b
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-09-11 19:42:36 +0200

    Created models for database

---
+++ WorkTracker/Models/Activity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTracker.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
