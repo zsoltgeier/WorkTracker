5bb5817080c315d6be37b390098b491e72974b48
geier.zsolt <zsoltgeier@stud.uni-obuda.hu>
2023-09-11 19:54:03 +0200

    Created model viewmodels

---
+++ WorkTracker/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTracker.ViewModels
+{
+    public class ActivityViewModel : ObservableObject
+    {
+        private int _id;
+        private string _title;
+        private string _description;
+        private TimeSpan _duration;
+        private string _comment;
+        private DateTime _date;
+        private int _employeeId;
+        private EmployeeViewModel _employee;
+
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        public TimeSpan Duration
+        {
+            get => _duration;
+            set => SetProperty(ref _duration, value);
+        }
+
+        public string Comment
+        {
+            get => _comment;
+            set => SetProperty(ref _comment, value);
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+            set => SetProperty(ref _date, value);
+        }
+
+        public int EmployeeId
+        {
+            get => _employeeId;
+            set => SetProperty(ref _employeeId, value);
+        }
+
+        public EmployeeViewModel Employee
+        {
+            get => _employee;
+            set => SetProperty(ref _employee, value);
+        }
+    }
+}
---
+++ WorkTracker/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTracker.ViewModels
+{
+    public class EmployeeViewModel : ObservableObject
+    {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+    }
+}
